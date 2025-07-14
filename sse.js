0b5d3ff53f0f0279870b222c2f64e9fdcbcb1af4
nova888 <nova.tampati@gmail.com>
2025-07-14 21:21:15 +0700

    tambah

--- sse.js
+++ sse.js
@@ -4,7 +4,7 @@ http.createServer((req, res) => if(req.url === '/event'){
     res.writeHead(200,
         'Content-Type' : 'text/event-stream',
         'Cache-Control' : 'no-cache',
-        'Connection'
+
     )
 }
 
